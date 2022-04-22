c943854d30229cdc2bc0e22e574f7c892931cbb4
frbar <fr.barbre@gmail.com>
2022-04-22 10:15:28 +0200

    refresh is bad

--- app/src/main/java/com/example/p2g2t222mini_project/DiceFragment.java
+++ app/src/main/java/com/example/p2g2t222mini_project/DiceFragment.java
@@ -1,9 +1,12 @@
 package com.example.p2g2t222mini_project;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +49,9 @@ public class DiceFragment extends Fragment {
             LogoRed.setVisibility(View.INVISIBLE);
             LogoGreen.setVisibility(View.VISIBLE);
             LogoBlue.setVisibility(View.INVISIBLE);
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.popBackStack();
+            navController.navigate(R.id.diceFragment);
 
         }
 
@@ -53,12 +59,18 @@ public class DiceFragment extends Fragment {
             LogoRed.setVisibility(View.VISIBLE);
             LogoGreen.setVisibility(View.INVISIBLE);
             LogoBlue.setVisibility(View.INVISIBLE);
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.popBackStack();
+            navController.navigate(R.id.diceFragment);
         }
 
         if(mainActivity.diceColorBlue == true) {
             LogoRed.setVisibility(View.INVISIBLE);
             LogoGreen.setVisibility(View.INVISIBLE);
             LogoBlue.setVisibility(View.VISIBLE);
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.popBackStack();
+            navController.navigate(R.id.diceFragment);
         }
 
         binding.DiceGreenButton.setOnClickListener(new View.OnClickListener() {
